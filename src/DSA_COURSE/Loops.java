587870d019f3c798d96dcbec8e85d49681dd5f12
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-04-21 17:12:54 +0530

    added

--- src/DSA_COURSE/Loops.java
+++ src/DSA_COURSE/Loops.java
@@ -8,10 +8,17 @@ public class Loops {
         Scanner sc= new Scanner(System.in);
         int num= sc.nextInt();
 
+        // For loop
         for (int i = 1; i < num+1; i++) {
-            System.out.println(i);
+            System.out.print(i+" ");
         }
 
+        //While Loop
+        int num1=1;
+        while (num1<=5){
+            System.out.println(num1);
+            num1++;
+        }
 
     }
 }
