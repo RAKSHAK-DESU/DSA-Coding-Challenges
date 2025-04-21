48b83adb5dad6342e71adb4d33b58c5951cdb1bb
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-04-21 16:59:34 +0530

    added

--- src/DSA_COURSE/Conditionals.java
+++ src/DSA_COURSE/Conditionals.java
@@ -4,9 +4,11 @@ public class Conditionals {
     public static void main(String[] args) {
         int salary= 100000;
 
-        if(salary>10000){
+        if(salary>10000 && salary<20000){
             salary+=2000;
-        }else{
+        } else if (salary>20000) {
+            salary+=3000;
+        } else{
             salary+=1000;
         }
         System.out.println(salary);
