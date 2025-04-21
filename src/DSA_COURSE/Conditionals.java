2a95c027fce7cdb7dfcb4acfe41b634e7dbd9360
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-04-21 16:56:12 +0530

    Added

---
+++ src/DSA_COURSE/Conditionals.java
@@ -0,0 +1,14 @@
+package DSA_COURSE;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        int salary= 100000;
+
+        if(salary>10000){
+            salary+=2000;
+        }else{
+            salary+=1000;
+        }
+        System.out.println(salary);
+    }
+}
