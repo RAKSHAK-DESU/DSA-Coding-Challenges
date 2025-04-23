2b5a1600b7b50f2b92cf993a35421b7eb0c35cbc
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-04-23 19:44:41 +0530

    added

--- src/DSA_COURSE/NestedSwitch.java
+++ src/DSA_COURSE/NestedSwitch.java
@@ -7,30 +7,19 @@ public class NestedSwitch {
         Scanner sc= new Scanner(System.in);
         int num = sc.nextInt();
         String Speciality= sc.next();
-        switch (num){
-            case 1:
-                System.out.println("Coder Chad");
-                break;
-            case 2:
+
+        switch (num) {
+            case 1 -> System.out.println("Coder Chad");
+            case 2 -> {
                 System.out.println("Rakuzan");
-                switch (Speciality){
-                    case "first":
-                        System.out.println("2nd life");
-                        break;
-                    case "Second":
-                        System.out.println("2nd life");
-                        break;
-
+                switch (Speciality) {
+                    case "first" -> System.out.println("2nd life");
+                    case "Second" -> System.out.println("2nd life");
                 }
-
-                break;
-            case 3:
-                System.out.println("Sukuna");
-                break;
-            case 4:
-                System.out.println("Kenji");
-                break;
-
+            }
+            case 3 -> System.out.println("Sukuna");
+            case 4 -> System.out.println("Kenji");
+            default -> throw new IllegalStateException("Unexpected value: " + num);
         }
     }
 }
