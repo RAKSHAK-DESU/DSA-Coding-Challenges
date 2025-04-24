1bd9428c2520c7290f0e9498e39359a33d2f0d17
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-04-24 17:45:11 +0530

    added

--- src/DSA_COURSE/Methods.java
+++ src/DSA_COURSE/Methods.java
@@ -5,12 +5,26 @@ import java.util.Scanner;
 public class Methods {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        System.out.print("Enter 1st number :");
+        System.out.println("enter 1st number");
         int num1= sc.nextInt();
-        System.out.print("Enter 2nd number :");
+        System.out.println("Enter 2nd number");
+        int num2=sc.nextInt();
+        int result =sum(num1,num2);
+        System.out.println(result);
+    }
+    static int sum(int a,int b){
+        int sum=a+b;
+        return sum;
+    }
+    static void add(){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Add 1st num:");
+        int num1= sc.nextInt();
+        System.out.print("Add 2nd num:");
         int num2= sc.nextInt();
         int sum= num1+num2;
         System.out.println(sum);
     }
+
 
 }
---
+++ src/DSA_COURSE/StringMethod.java
@@ -0,0 +1,13 @@
+package DSA_COURSE;
+
+public class StringMethod {
+    public static void main(String[] args) {
+         String lord =Greet();
+        System.out.println(lord);
+    }
+    static String Greet(){
+        String name="Rakshak";
+        return name;
+    }
+
+}
