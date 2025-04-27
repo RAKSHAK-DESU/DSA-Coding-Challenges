655ff2d5b938f10e540dccb091dc503dff803968
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-04-27 23:01:40 +0530

    Added

---
+++ src/MethodHomework/Greeting.java
@@ -0,0 +1,20 @@
+package MethodHomework;
+
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter Your Name : ");
+        String name= sc.next();
+
+        String greet= greeting(name);
+        System.out.println(greet);
+    }
+    static String greeting(String name){
+        String str= "Hello Mr/Ms "+name+" Good Morning";
+        return str;
+    }
+
+
+}
