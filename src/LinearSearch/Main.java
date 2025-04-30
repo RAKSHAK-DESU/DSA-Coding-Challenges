f0280f679abde679bdc727d06a5674eaee87ed9a
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-04-30 20:29:02 +0530

    added

--- src/LinearSearch/Main.java
+++ src/LinearSearch/Main.java
@@ -4,9 +4,23 @@ public class Main {
     public static void main(String[] args) {
         int[] nums={12,43,12,467,76,45,23,76,34,2398,239,2398,89,745};
         int target=239;
-        ;
+
         System.out.println(LinearSearch(nums,target));
+        System.out.println(ElementReturn(nums,target));
     }
+
+    static int ElementReturn(int[] arr,int target){
+        if (arr.length==0){
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==target){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+
     static int LinearSearch(int[] arr, int target){
         if (arr.length==0){
             return -1;
