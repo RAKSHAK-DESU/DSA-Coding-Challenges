e4a0a058cc80e262fb459a9c4792ae2b7a7da84b
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-04-30 20:55:06 +0530

    added

--- src/LinearSearch/Main.java
+++ src/LinearSearch/Main.java
@@ -7,6 +7,19 @@ public class Main {
 
         System.out.println(LinearSearch(nums,target));
         System.out.println(ElementReturn(nums,target));
+        System.out.println(ExistOrNot(nums,target));
+    }
+
+    static boolean ExistOrNot(int[] arr, int target){
+        if (arr.length==0){
+            return false;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==target){
+                return true;
+            }
+        }
+        return false;
     }
 
     static int ElementReturn(int[] arr,int target){
