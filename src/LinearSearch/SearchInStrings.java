f621dc1a7331bca978a87d1da69631764f0a3f9f
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-04-30 20:58:56 +0530

    added

--- src/LinearSearch/SearchInStrings.java
+++ src/LinearSearch/SearchInStrings.java
@@ -2,8 +2,27 @@ package LinearSearch;
 
 public class SearchInStrings {
     public static void main(String[] args) {
-
+        String str="RAKSHAK IS KING , HE IS SON OF TOPG";
+        char target='K';
+        boolean result =Search(str,target);
+        System.out.println(result);
     }
+   // static boolean Search1(){
+
+ //   }
+
+
+
+
+
+
+
+
+
+
+
+
+
     static boolean Search(String str, char target){
         if (str.length()==0){
             return false;
