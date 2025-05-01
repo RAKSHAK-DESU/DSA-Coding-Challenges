23ba57b5128a23ef17ed2e32e193d9fdf84c1f53
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-05-01 13:39:04 +0530

    added

---
+++ src/MethodHomework/PalindromeNum.java
@@ -0,0 +1,35 @@
+package MethodHomework;
+
+import java.util.Scanner;
+
+public class PalindromeNum {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number");
+        int original= sc.nextInt();
+
+
+        System.out.println(isPalindrome( original,palindrom(original)));
+
+    }
+    static int palindrom(int original){
+        int reversed=0;
+        while (original>0){
+            int lastDigit= original%10;
+            reversed=reversed*10+lastDigit;
+            original=original/10;
+
+        }
+        return reversed;
+    }
+
+    static boolean isPalindrome(int original, int reversed){
+        boolean palNum=false;
+        if (original==reversed){
+            palNum=true;
+        }
+        return palNum;
+    }
+
+
+}
