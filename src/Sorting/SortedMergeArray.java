4ae447cddd87d5306165572ad07f544185345176
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-05-25 19:15:46 +0530

    added

---
+++ src/Sorting/SortedMergeArray.java
@@ -0,0 +1,26 @@
+package Sorting;
+//https://leetcode.com/problems/merge-sorted-array/
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        for(int i=0;i<nums1.length-m;i++){
+            nums1[m+i]=nums2[i];
+        }
+        //now we have nums1
+
+        for(int i=0;i<nums1.length;i++){
+
+            for(int j=1;j<nums1.length;j++){
+                //swap if the item is smaller than previous item
+                if(nums1[j]<nums1[j-1]){
+                    //swap
+                    int temp=nums1[j-1];
+                    nums1[j-1]=nums1[j];
+                    nums1[j]=temp;
+                }
+            }
+        }
+
+
+    }
+}
